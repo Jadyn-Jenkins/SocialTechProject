96d5c16ebdfef7c079721c061d3f28a9cc76776c
sbhandari1992 <sambhandari2021@yahoo.com>
2022-12-23 09:51:37 -0500

    add settime function to call the function on order, sucessful to send and get data with the same button (post)

--- profile/profile.js
+++ profile/profile.js
@@ -1,28 +1,46 @@
 
-// const loginData = JSON.parse(window.localStorage.getItem("login-data"))
-//     const options = { 
-//         method: "GET",
-//         headers: {Authorization: `Bearer ${loginData.token}`},
-//     };
+document.querySelector('#profileForm').addEventListener('submit', collectAndSendData);
+document.querySelector('#goToFeed').addEventListener('click', getAllUsers);
 
-//     fetch(endpoint, options)
+const endpoint = 'https://microbloglite.herokuapp.com/api/posts'
+const loginData = JSON.parse(window.localStorage.getItem("login-data"))
+const options = { 
+    method: "GET",
+    headers: {Authorization: `Bearer ${loginData.token}`},
+};
 
-
-let postElement = document.getElementById('post');
-console.log(postElement);
-
-postElement.addEventListener('submit', collectAndSendData);
-
+let formJSON;
 function collectAndSendData(evt) {
     console.log('Collecting data .........................');
-    
+    evt.preventDefault();
+    const data = new FormData(evt.target);
+    console.log(data.entries()); 
+    formJSON = Object.fromEntries(data.entries());
+    console.log(formJSON);
+    // postFormData();
+    // getAllUsers();
+    setTimeout(postFormData, 1000);
+    setTimeout(getAllUsers, 2000);
 }
 
-// function getAndSendData(evt) {
-//     evt.preventDefault();
-//     const data = new FormData(evt.target);
-//     console.log(data.entries());   // transform a list to key value pairs
-//     formJSON = Object.fromEntries(data.entries());
-//     console.log(formJSON);
-//    postData(); 
-// }
+function postFormData() {
+    fetch(endpoint,{
+        method: "POST",
+        body: JSON.stringify(formJSON),
+        headers:{"Content-type": "application/json; charset=UTF-8", Authorization: `Bearer ${loginData.token}`}
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => {
+        console.log(error);
+        'Unexpected Error';
+    })
+}
+
+function getAllUsers() {
+    fetch(endpoint,options)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+
