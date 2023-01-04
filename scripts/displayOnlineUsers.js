8488706a27549fe4c0ad52c47d306a09cf6b5301
sbhandari1992 <sambhandari2021@yahoo.com>
2023-01-03 20:41:39 -0500

    create some html div,id and alt and being able to compare the time and show user as online if they post somthing

--- scripts/displayOnlineUsers.js
+++ scripts/displayOnlineUsers.js
@@ -26,6 +26,36 @@ export function showOnlinePerson() {
             let postTimeMilSec = new Date(actualPostTime).getTime();
             let currentTime = new Date();
             let miliSecCurrentTime = currentTime.getTime();
+            if ( postTimeMilSec > miliSecCurrentTime - miliSecToSee) {
+                //creating html element with javascript
+                const childElement = document.createElement("div");
+                childElement.classList.add("child");
+                const imgElement = document.createElement("img");
+                imgElement.id = "onlineProfile";
+               
+                //     <img src="../img/online-green.jpeg" alt="">
+                // <img src="../img/online-green.jpeg" alt="">
+                 // <img src="../img/user-286.png" alt=""></img>
+               
+                imgElement.src = "../img/user-286.png";
+                imgElement.alt = "user picture";
+                childElement.appendChild(imgElement);
+                const pElement = document.createElement("p");
+                pElement.innerText = `${arrItem.username}`;
+                childElement.appendChild(pElement);
+                const imgTagOnline = document.createElement("img");
+                imgTagOnline.id = "onlineIcon";
+               
+               
+                   // imgTagOnline.src = "../images/offline-red.png";
+                    // imgTagOnline.alt = "red offline picture";
+               
+                    imgTagOnline.src = "../img/online-green.jpeg";
+                     imgTagOnline.alt = "green-online-picture";
+               
+                    childElement.appendChild(imgTagOnline);
+                    onlineUsesr.appendChild(childElement);
+                }
             
         });
     });
